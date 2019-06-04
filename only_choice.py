4199274ada98c0419b7921f5107d8aca5d86bdd7
annegsilva <30444315+annegsilva@users.noreply.github.com>
2019-06-04 12:48:31 -0300

    Update only_choice.py

--- only_choice.py
+++ only_choice.py
@@ -1,3 +1,30 @@
+# Helper function - Make a string with the already allocated elements of a block
+def allocated_elements(board, selected_rows, selected_cols):
+	allocated_elements = ''
+	positions = [r+c for r in selected_rows for c in selected_cols]
+	for position in positions:
+		if(len(board[position]) == 1):
+			allocated_elements += board[position]
+
+	return allocated_elements	
+
+
+# Helper function - update the positions with more than one value with the new allocated elements
+def update_missing_elements_block(board, selected_rows, selected_cols):
+	block_elements = allocated_elements(board, selected_rows, selected_cols)
+	positions = [r+c for r in selected_rows for c in selected_cols]
+	for position in positions:
+		if (len(board[position]) > 1):
+			for element in board[position]:
+				if (element in block_elements):
+					board[position] = board[position].replace(element, '')
+
+# Helper function - update all grid positions with more than one value after the elimination technique
+def update_all_missing_elements(board, block_rows, block_cols):
+	for r in block_rows:
+		for c in block_cols:
+			update_missing_elements_block(board,r,c)
+
 #Helper Function - Returns True if the index element is unique in the string
 def is_unique(elements_string, element_index):
 	
@@ -29,6 +56,6 @@ def find_only_choice_block(board, selected_rows, selected_cols):
 def all_only_choices(board, block_rows, block_cols):
 	for r in block_rows:
 		for c in block_cols:
-			block_only_choices(board,r,c)
+			find_only_choice_block(board,r,c)
 
 	return board
