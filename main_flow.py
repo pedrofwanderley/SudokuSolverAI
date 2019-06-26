ddaa3ec7d5915fef6d16e99e6359a3295099c790
pedrofwanderley <pedro.wanderley@ccc.ufcg.edu.br>
2019-06-25 23:59:10 -0300

    adcionando função de comparação de tempo de execução

--- main_flow.py
+++ main_flow.py
@@ -1,6 +1,7 @@
 import elimination
 import only_choice
 import grids
+import time
 
 # Gri's coordinates
 rows = 'ABCDEFGHI'
@@ -30,17 +31,24 @@ def create_dict_representation(board, rows, cols):
 
 # Creates a string representation with rows and columns for the board
 def board_to_string(board, rows, cols):
-	string = ''
+	string = '------|-------|--------\n'
 	positions = [r+c for r in rows for c in cols]
 	count = 0
+	count2 = 0
 	for position in positions:
-		string += board[position] + ' '
 		count += 1
+		if (count % 3 != 0):
+			string += board[position] + ' '
+		else:
+			string += board[position] + " | "
 		if (count == 9) :
 			string += '\n'
 			count = 0
-
+			count2 += 1
+			if (count2 % 3 == 0):
+				string += "------|-------|--------\n"
 	return string
+
 
 def level_resolution(boards_list, level):
 	for b in boards_list:
@@ -48,14 +56,14 @@ def level_resolution(boards_list, level):
 		board = create_dict_representation(b, rows, cols)
 		elimination.fill_all_dot_cells(board,rows, cols, elements_string)
 		print(board_to_string(board,rows,cols))
-		
-		if (level == 'SE'): 
+
+		if (level == 'SE'):
 			board_updated = elimination.elimination_technique(board, rows, cols, block_rows, block_cols)
 			print(board_to_string(board_updated, rows, cols))
-		if (level == 'E'): 
+		if (level == 'E'):
 			board_updated = elimination.elimination_technique(board, rows, cols, block_rows, block_cols)
 			print(board_to_string(board_updated, rows, cols))
-		if (level == 'M'): 
+		if (level == 'M'):
 			board_updated = elimination.elimination_technique(board, rows, cols, block_rows, block_cols)
 			print(board_to_string(board_updated, rows, cols))
 		print('\n>> ---------------------------------- << \n')
@@ -72,10 +80,10 @@ def level_resolution(boards_list, level):
 # 		board_updated = elimination.eliminate_peer(board,rows,cols,empty_positions_list)
 # 		empty_positions_list = elimination.empty_positions(board_updated,rows, cols)
 # 		count += 1
-	
+
 # 	print(board_to_string(board_updated,rows,cols))
 # 	print(check_solution(board_updated, rows, cols))
-	
+
 # 	if(check_solution(board_updated, rows, cols) == False):
 # 		count = 0
 # 		while(count <= 2):
@@ -90,11 +98,11 @@ def level_resolution(boards_list, level):
 # 			board_updated = elimination.eliminate_peer(board,rows,cols,empty_positions_list)
 # 			empty_positions_list = elimination.empty_positions(board_updated,rows, cols)
 # 			count += 1
-		
+
 # 	print(board_to_string(board_updated,rows,cols))
-	
+
 # 	print('\n>> ---------------------------------- << \n')
-	
+
 # Helper function - Returns an boolean value, True if all positions have only one value alocated and False otherwise
 def check_solution(board, rows, cols):
 	positions = [r+c for r in rows for c in cols]
@@ -104,6 +112,20 @@ def check_solution(board, rows, cols):
 
 
 	return True
+
+def compareSolutionsTimes(func1, func2):
+
+	ini_func1 = time.time()
+	func1(1, 1000000)
+	fim_func1 = time.time()
+	print "Função 1: ", fim_func1-ini_func1
+
+	# verifica o tempo de resposta da função soma2
+	ini_func2 = time.time()
+	soma2(1, 1000000)
+	fim_func2 = time.time()
+	print "Função 2: ", fim_func2-ini_func2
+
 
 #To see the elimination technique flow with the Super Easy Boards
 level_resolution(grids.super_easy_boards, 'SE')
